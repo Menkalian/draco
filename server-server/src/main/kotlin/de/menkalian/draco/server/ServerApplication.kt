07806388a6757a941347933c1d7dd86fa82b4f5c
Kilian Krampf <kilian@krampf.de>
2022-04-04 20:43:50 +0200

    Configure CORS

--- server-server/src/main/kotlin/de/menkalian/draco/server/ServerApplication.kt
+++ server-server/src/main/kotlin/de/menkalian/draco/server/ServerApplication.kt
@@ -1,15 +1,23 @@
 package de.menkalian.draco.server
 
-import org.springframework.boot.CommandLineRunner
+import de.menkalian.draco.server.util.logger
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class ServerApplication {
     @Bean
-    fun testRunner() : CommandLineRunner {
-        return CommandLineRunner {
+    fun corsConfiguration() = object : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            super.addCorsMappings(registry)
+            logger().info("Configuring CORS")
+            registry
+                .addMapping("/**")
+                .allowedMethods("GET", "PUT", "POST", "DELETE")
+                .allowedOrigins("*")
         }
     }
 }
